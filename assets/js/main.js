cc7f5ac058a90d10cd19d5526752c73a791ed984
Front-end-4 <waggner.dev@gmail.com>
2025-04-08 11:15:38 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,13 +120,6 @@
   });
 
   /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
-
-  /**
    * Init isotope layout and filters
    */
   document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
